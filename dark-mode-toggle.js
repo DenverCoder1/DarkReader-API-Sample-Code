6f3405f05f3a50c2fc4d3398667166fb75e750b1
eyl327 <jonah@freshidea.com>
2020-07-30 15:45:44 +0300

    Improved code formatting and added comments

--- dark-mode-toggle.js
+++ dark-mode-toggle.js
@@ -1,37 +1,58 @@
-var darkModeModification = `<style class='dark-mode-mod'>
-.navbar-brand img,
-.footer-widget img {
-    filter: invert(1);
-}
-</style>`
-
+// function to toggle dark mode
+// turn on if newState is "on", otherwise, turn off dark mode
 function toggleDarkMode(newState) {
+    // style element to insert into the page when dark mode is enabled
+    // (fixes the logo color to be white in dark mode)
+    var darkModeModification = `<style class='dark-mode-mod'>
+            .navbar-brand img,
+            .footer-widget img {
+                filter: invert(1);
+            }
+        </style>`;
+    // turning on dark mode
     if (newState == "on") {
-        DarkReader.enable({contrast: 150});
+        // enable dark mode using the DarkReader API
+        DarkReader.enable({ contrast: 150 });
+        // set the toggle button to have the sun icon
         document.querySelector("div.dark-mode-toggle").firstElementChild.className = "gg-sun";
+        // insert style element to apply some minor adjustments
         document.head.insertAdjacentHTML("beforeend", darkModeModification);
-        setCookie("darkmode","on",9999);
+        // set cookie so that preference will be remembered
+        setCookie("darkmode", "on", 9999);
     }
+    // turning off dark mode
     else {
+        // disable dark mode using the DarkReader API
         DarkReader.disable();
+        // set the toggle button to have the moon icon
         document.querySelector("div.dark-mode-toggle").firstElementChild.className = "gg-moon";
-        var cssMod = document.getElementsByClassName("dark-mode-mod")[0];
+        // find the style element with the modifications
+        var cssMod = document.querySelector(".dark-mode-mod");
+        // if the style element is on the page
         if (cssMod) {
+            // remove the style element
             cssMod.parentElement.removeChild(cssMod);
         }
-        setCookie("darkmode","off",9999);
+        // set cookie so that preference will be remembered
+        setCookie("darkmode", "off", 9999);
     }
 }
 
+// add an event listener to detect clicking on the dark mode toggle button
 document.querySelector("div.dark-mode-toggle").addEventListener("click", function () {
+    // if there is an element with the class "darkreader" on the page (DarkReader is enabled)
     if (document.querySelector(".darkreader")) {
+        // turn off dark mode
         toggleDarkMode("off");
     }
     else {
+        // turn on dark mode
         toggleDarkMode("on");
     }
 }, false);
 
+// code from w3schools to set a cookie (https://www.w3schools.com/js/js_cookies.asp)
+// parameters: name of the cookie, value to set, number of days until it expires
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -39,6 +60,8 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+// code from w3schools to get the value of a cookie (https://www.w3schools.com/js/js_cookies.asp)
+// parameters: name of the cookie
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -54,4 +77,13 @@ function getCookie(cname) {
     return "";
 }
 
-toggleDarkMode(getCookie("darkmode"));
+// when the page loads, toggle dark mode according to the cookie settings
+window.addEventListener("load", function () {
+    // get the dark mode cookie value
+    var darkModeCookie = getCookie("darkmode");
+    // if the cookie is set to "on", turn on dark mode
+    if (darkModeCookie == "on") {
+        // toggle dark mode
+        toggleDarkMode(darkModeCookie);
+    }
+}, false);
